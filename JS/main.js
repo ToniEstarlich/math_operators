043391e6a6e53c67423244183a8857d3566b28e6
Antonio Estarlich <110102891+ToniEstarlich@users.noreply.github.com>
2022-12-08 11:57:48 +0000

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,48 @@
+function addition_Function() { //Defining a addition function and naming 
+    var addition= 2 + 2;//Defining a addition 
+    document.getElementById("Addition_Function").innerHTML= "2 + 2 =" + addition;//Putting the subtraction 
+} 
+
+function subtraction_Function() { //Defining a substraction addition and naming
+    var subtraction = 5 - 2 ;//Defining a subtraction
+    document.getElementById("subtraction_Function").innerHTML = "5 - 2 =" + subtraction;//Putting the subtraction
+}
+
+function multiplication() { //Defining a Multiplication function and naming 
+    var simple_Math= 6 * 8;//Defining a simple Math
+    document.getElementById("multiplication").innerHTML= "6 * 8 =" +simple_Math;//Putting the subtraction
+} 
+
+
+function division() { //Defining a substraction addition and naming
+    var simple_Math = 48 / 6 ;//Defining a simple math
+    document.getElementById("division").innerHTML = "48 / 6 =" + subtraction;//Putting the substraction
+}
+
+function more_Math() { //Defining a substraction more Maths
+    var simple_Math = (1 + 2) * 10 / 2 -5 ;//Defining a simple math
+    document.getElementById("more_Math").innerHTML = " 1 plus 2, multiplied by 10 divided in half and then substracted by 5 equals " + simple_Maths; //Putting the multiply substract
+}
+
+function modulus_Operator() { //Defining a substraction of Modulus Operator
+    var simple_Math = 25 % 6 ;//Defining a simple math
+    document.getElementById("modulus_Operator").innerHTML = "When you divide 25 by 6 you have a remainder of:" + simple_Math;//Putting the divide substraction 
+}
+
+function negation_Operator () {//Defining a substraction of negation Operator
+    var x = 10;//Defining a variable 
+    document.getElementById("negation_Operator").innerHTML= -x;//Putting the -X
+}
+
+var X = 5;//Defining to increment
+x++;//written ++ counts one step up
+document.write(X);//Putting the (X)
+
+var X =5.25;//Definig the Decrement
+X--;//written decrease or reduce
+document.write(X);//Putting the (X)
+
+windows.alert(Math.random());//return a random number betweeen 0 and 1
+ windows.alert(math.random()*100)//Random number displayed between 0 and 100
+
+ 
